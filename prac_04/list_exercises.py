ae147211bf31052975f348e897f5fd3ef00e5c6f
jc320054 <lukas.arnold@my.jcu.edu.au>
2019-03-31 16:19:09 +1000

    completed list exercises

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,15 +3,28 @@ CP1404/CP5632 Practical
 Basic list operations code
 """
 
+# 1. Basic list operations
 
-numbers = []
+# numbers = []
+#
+# for i in range(5):
+#     numbers.append(int(input("Number: ")))
+#
+# print("The first number is: {}".format(numbers[0]))
+# print("The last number is: {}".format(numbers[-1]))
+# print("The smallest number is: {}".format(min(numbers)))
+# print("The largest number is: {}".format(max(numbers)))
+# print("The average of the numbers is: {}".format(sum(numbers) / 5))
 
-for i in range(5):
-    numbers.append(int(input("Number: ")))
+# 2. Woefully inadequate security checker
 
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
+             'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole',
+             'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter Username: ")
 
-print("The first number is: {}".format(numbers[0]))
-print("The last number is: {}".format(numbers[-1]))
-print("The smallest number is: {}".format(min(numbers)))
-print("The largest number is: {}".format(max(numbers)))
-print("The average of the numbers is: {}".format(sum(numbers)/5))
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
