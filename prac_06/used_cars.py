af077ce1f4380b676b1751cc5f9ba6e619a35c07
jc320054 <lukas.arnold@my.jcu.edu.au>
2019-04-09 09:28:27 +1000

    add name and limo

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -15,5 +15,14 @@ def main():
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
+    # Modifications
+    print(" ")
+    limo = Car(100)
+    limo.add_fuel(20)
+    print("Limo fuel =", limo.fuel)
+    limo.drive(115)
+    print("Limo odo =", limo.odometer)
+    print(limo)
+
 
 main()
