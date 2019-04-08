5a8562228efa3c584f83f13b10c95d0c16f91f03
jc320054 <lukas.arnold@my.jcu.edu.au>
2019-04-08 13:03:45 +1000

    finish code without sorting

--- prac_05/word_ocurrences.py
+++ prac_05/word_ocurrences.py
@@ -9,14 +9,19 @@ string = input("Enter a string: ")
 # Create empty dictionary
 words_and_counts = {}
 
-# Split string into words and store words as keys in dictionary
+# Split string into words and store in a list of words
 words = string.split(" ")
 
 # Check and count how many times a word is in words
-word_counter = 1
-for word in words:
-    if words[word] == words[word + 1]:
-        word_counter += 1
+word_counter = 0
+for i in range(len(words)):
+    for j in range(len(words)):
+        if words[i] == words[j]:
+            word_counter += 1
+    words_and_counts['{}'.format(words[i])] = word_counter
+    word_counter = 0
+
+
 
 # Find longest word
 for words, counts in words_and_counts.items():
@@ -26,4 +31,4 @@ for words, counts in words_and_counts.items():
 print("Text: {}".format(string))
 
 for words, counts in words_and_counts.items():
-    print("{:{}} = {}".format(x, y, z))
+    print("{:{}} = {}".format(words, len(longest_word), counts))
