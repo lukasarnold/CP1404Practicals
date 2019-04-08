38b237f82dfbe51236e9a5786738ad4ff6974e83
jc320054 <lukas.arnold@my.jcu.edu.au>
2019-04-09 09:53:06 +1000

    create programming language class and file

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -3,3 +3,13 @@
 
 class ProgrammingLanguage:
     """Represent a programming language object."""
+
+    def __init__(self, language=""):
+        """Initialise the Programming Language Instance
+
+        language: string, name of programming language"""
+        self.language = language
+
+    def __str__(self):
+        """Return the representation of the programming language object"""
+        return "{}".format(self.language)
