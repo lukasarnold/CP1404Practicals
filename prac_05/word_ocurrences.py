ebdd21b069f689c814067fcb232ac2d4bf611b97
jc320054 <lukas.arnold@my.jcu.edu.au>
2019-04-08 13:23:04 +1000

    add sorting and check output

--- prac_05/word_ocurrences.py
+++ prac_05/word_ocurrences.py
@@ -1,6 +1,8 @@
 """
 CP1404/CP5632 Practical
 Word occurrences in a dictionary
+
+Use sample input: this is a collection of words of nice words this is a fun thing is it
 """
 
 # Ask for user input
@@ -12,23 +14,22 @@ words_and_counts = {}
 # Split string into words and store in a list of words
 words = string.split(" ")
 
+# Sort the dictionary by alphabetic order of words
+words.sort()
+
+# Find longest word and length
+length_longest_word = max(len(word) for word in words)
+
 # Check and count how many times a word is in words
-word_counter = 0
 for i in range(len(words)):
+    word_counter = 0
     for j in range(len(words)):
         if words[i] == words[j]:
             word_counter += 1
     words_and_counts['{}'.format(words[i])] = word_counter
-    word_counter = 0
 
-
-
-# Find longest word
-for words, counts in words_and_counts.items():
-    longest_word = max(words)
 
 # Print output
 print("Text: {}".format(string))
-
 for words, counts in words_and_counts.items():
-    print("{:{}} = {}".format(words, len(longest_word), counts))
+    print("{:{}} : {}".format(words, length_longest_word, counts))
