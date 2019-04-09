97bf5b78b38e634ded752b8e3830a93c61851c66
jc320054 <lukas.arnold@my.jcu.edu.au>
2019-04-09 10:19:29 +1000

    add str method for string return

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -7,9 +7,17 @@ from prac_06.programming_language import ProgrammingLanguage
 def main():
     """Code to see how class ProgrammingLanguages works"""
 
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    java = ProgrammingLanguage("Java", "Static", True, 1995)
+    c_plus_plus = ProgrammingLanguage("c_plus_plus", "Static", False, 1983)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+
+    print(java)
+    print(c_plus_plus)
+    print(ruby)
+    print(python)
+    print(visual_basic)
 
 
 main()
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,15 +4,24 @@
 class ProgrammingLanguage:
     """Represent a programming language object."""
 
-    def __init__(self, language=""):
+    def __init__(self, language="", typing="", reflection="", year=0):
         """Initialise the Programming Language Instance
 
-        language: string, name of programming language"""
+        language: string, name of programming language
+        typing: string,
+        reflection: boolean,
+        year: integer,
+        """
+
         self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
 
     def __str__(self):
         """Return the representation of the programming language object"""
-        return "{}".format(self.language)
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.language, self.typing,
+                                                                             self.reflection, self.year)
 
     def is_dynamic(self):
         if self == "Java" or "C++" or "Visual Basic":
