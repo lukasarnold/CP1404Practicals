2de0eb0535796a1910822bddf0bcfcc9319c92af
jc320054 <lukas.arnold@my.jcu.edu.au>
2019-04-09 10:23:10 +1000

    create guitar_test file

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,11 @@
+"""CP1404/CP5632 Practical - Client code to use the Guitar class."""
+# Note that the import has a folder (module) in it.
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Code to see how class Guitar works"""
+
+
+main()
