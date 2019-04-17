49f5a6cfd14860b9938b21632eda877717f9d67c
jc320054 <lukas.arnold@my.jcu.edu.au>
2019-04-17 12:52:35 +1000

    add code for guitars client code

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,4 +1,4 @@
-"""CP1404/CP5632 Practical - Client code to use the Guitar class."""
+"""CP1404/CP5632 Practical - Test code for the Guitar class."""
 # Note that the import has a folder (module) in it.
 
 from prac_06.guitar import Guitar
---
+++ prac_06/guitars.py
@@ -0,0 +1,48 @@
+"""CP1404/CP5632 Practical - Client code to use the Guitar class."""
+# Note that the import has a folder (module) in it.
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Program that uses Guitar class."""
+
+    # Create empty list to store all guitars
+    guitars = []
+
+    # Print welcome message
+    print("My Guitars!")
+
+    # Ask for inputs and call class then store in list
+    name = input("Name:")
+    while name != "":
+        year = int(input("Year:"))
+        cost = float(input("Cost: $"))
+
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print("{} added".format(new_guitar))
+
+        name = input("Name:")
+
+    # Add two guitars you already own
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    # Print the guitars you own
+    print("These are my Guitars:")
+    for guitar in range(len(guitars)):
+
+        name = guitars[guitar].name
+        year = guitars[guitar].year
+        cost = guitars[guitar].cost
+
+        vintage_status = ""
+        if guitar.is_vintage():
+            vintage_status = "(vintage)"
+
+        print("Guitar {}: {} ({}), worth ${} {}".format(guitar + 1, name, year, cost, vintage_status)
+
+
+
+main()
