3e88890d49488aa80dfa6674e6b2e58b5433117b
jc320054 <lukas.arnold@my.jcu.edu.au>
2019-05-16 12:18:28 +1000

    finish taxi test

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,19 @@
+""""
+Taxi Test Code
+Lukas Arnold
+"""
+
+from prac_08.taxi import Taxi
+
+
+def main():
+    """Test Taxi class."""
+    my_taxi = Taxi("Prius 1", 100, 5)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+
+
+main()
